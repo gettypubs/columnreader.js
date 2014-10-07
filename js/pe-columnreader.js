c80fc19e37a0f958de0b86b48709a2ba35e42ccb
Greg Albers <geealbers@gmail.com>
2014-10-07 09:12:33 -0700

    Loading original files

---
+++ js/pe-columnreader.js
@@ -0,0 +1,69 @@
+/* ------ Change these to adjust your layout ------ */
+
+var col = 380;  /* column width */
+var ht = 560; /* column height */
+var gap = 20;  /* gap between columns */
+
+/* Delete numbers to set a maximum number of columns */
+
+var cols = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+var t = document.getElementById('text');
+var f = document.getElementById('textframe');
+var p = document.getElementById('prev');
+var n = document.getElementById('next');
+var r = document.getElementById('reader');	
+var x = col + gap;		
+var controls = p.offsetWidth + n.offsetWidth;
+function setLayout() {
+	p.style.marginTop = ht * .4  + "px";
+	n.style.marginTop = ht * .4  + "px";	
+	f.style.height = ht + 20 + "px";
+	f.style.width = col + "px";
+	t.style.height = ht + "px";
+	t.style.columnWidth = col + "px";
+	t.style.MozColumnWidth = col + "px";
+	t.style.WebkitColumnWidth = col + "px";
+	t.style.columnGap = gap + "px";
+	t.style.MozColumnGap = gap + "px";
+	t.style.WebkitColumnGap = gap + "px";
+	checkWidth();		
+	};				
+function checkWidth() { 	
+	var w = window.innerWidth;	
+	var avail = w - controls;
+	var b = avail / x;
+	var colnum = Math.floor(b);
+	for ( i = 0; i < cols.length; i++) {		
+		if ( cols[i] <= colnum) { 
+		f.style.width= (cols[i] * x) - gap + "px"; 
+		r.style.width= (cols[i] * x) - gap + controls + "px"; }
+		}
+	};	
+function scrollNext() {
+	var xtr = f.scrollLeft % x;
+	for ( i = 0; i < cols.length; i++) {
+		if ( f.style.width==((cols[i] * x) - gap) + "px" ) {
+		f.scrollLeft = f.scrollLeft + ((cols[i] * x)-xtr);}
+		}
+	};	
+function scrollEnd() {
+	for ( i = 0; i < cols.length; i++) {
+		if ( f.style.width==((cols[i] * x) - gap) + "px" ) {
+		if ( f.scrollLeft >= (f.scrollWidth - ((cols[i] * x) - gap)) ) { window.location.href = nx; }
+		else { f.scrollLeft = f.scrollWidth - ((cols[i] * x) - gap); } }
+		}
+	};				
+function scrollPrev() {
+	var xtr = f.scrollLeft % x;			
+	if ( xtr == 0 ) { var y = 0; }
+	else { var y = x - xtr; };
+	for ( i = 0; i < cols.length; i++) {
+		if ( f.style.width==((cols[i] * x) - gap) + "px" ) {
+		f.scrollLeft = f.scrollLeft - ((cols[i] * x)-y);}
+		}
+	};							
+function scrollBeginning() {
+	if ( f.scrollLeft == 0 ) { window.location.href = pr; }
+	{ f.scrollLeft = 0; }
+	};
